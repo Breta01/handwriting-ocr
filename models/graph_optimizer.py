8370aaaf9b7673f0cc1e64124a3f3fb250550df3
Breta01 <breta.hajek@gmail.com>
2017-09-17 20:30:28 +0200

    Updating graph optimizer to work with Tensorflow's graph transform

--- models/graph_optimizer.py
+++ models/graph_optimizer.py
@@ -1,18 +1,17 @@
 """
 Usage:
 python graph_optimizer.py \
---model folder/model_name \
+--tf_path ../../tensorflow/ \
+--model_folder "path_to_the_model_folder" \
 --output_names "activation, accuracy" \
 --input_names "x"
 """
 
 import os, argparse
+from subprocess import call
 
 import freeze_graph
 import tensorflow as tf
-from tensorflow.python.framework import graph_util
-from tensorflow.python.platform import gfile
-from tensorflow.python.tools import optimize_for_inference_lib
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -21,47 +20,14 @@ op_name = "_optimized.pb"
 
 
 def graph_freez(model_folder, output_names):
-    """
-    # Retrieve checkpoint fullpath
+    print("Model folder", model_folder)
     checkpoint = tf.train.get_checkpoint_state(model_folder)
-    input_checkpoint = checkpoint.model_checkpoint_path
-    output_graph = input_checkpoint + fr_name
-
-    output_node_names = output_names
-
-    # Import the meta graph and retrieve a Saver
-    clear_devices = True
-    saver = tf.train.import_meta_graph(input_checkpoint + '.meta',
-                                       clear_devices=clear_devices)
-
-    # Retrieve the protobuf graph definition
-    graph = tf.get_default_graph()
-    input_graph_def = graph.as_graph_def()
-
-    # Start a session and restore the graph weights
-    with tf.Session() as sess:
-        saver.restore(sess, input_checkpoint)
-
-        output_graph_def = graph_util.convert_variables_to_constants(
-            sess,
-            input_graph_def,
-            output_node_names.split(",")
-        ) 
-
-        with gfile.GFile(output_graph, "wb") as f:
-            f.write(output_graph_def.SerializeToString())
-        print("%d ops in the frozen graph." % len(output_graph_def.node))
-
-    return output_graph
-    """
-    checkpoint = tf.train.get_checkpoint_state(model_folder)
+    print(checkpoint)
     checkpoint_path = checkpoint.model_checkpoint_path
     output_graph_filename = checkpoint_path + fr_name
-    print(checkpoint)
-    
+
     input_saver_def_path = ""
     input_binary = True
-#    checkpoint_path = model_folder
     output_node_names = output_names
     restore_op_name = "save/restore_all"
     filename_tensor_name = "save/Const:0"
@@ -72,35 +38,33 @@ def graph_freez(model_folder, output_names):
         "", input_saver_def_path, input_binary, checkpoint_path,
         output_node_names, restore_op_name, filename_tensor_name,
         output_graph_filename, clear_devices, "", "", input_meta_graph)
-    
+
     return output_graph_filename
-  
 
-def graph_optimization(graph_file, input_names, output_names):
+
+def graph_optimization(tf_path, graph_file, input_names, output_names):
     output_file = graph_file[:-len(fr_name)] + op_name
+    tf_path += "bazel-bin/tensorflow/tools/graph_transforms/transform_graph"
 
-    input_graph_def = tf.GraphDef()
-    with gfile.Open(graph_file, "rb") as f:
-        data = f.read()
-        input_graph_def.ParseFromString(data)
-
-    output_graph_def = optimize_for_inference_lib.optimize_for_inference(
-        input_graph_def,
-        input_names.split(","),
-        output_names.split(","),
-        tf.float32.as_datatype_enum)
-
-    with gfile.FastGFile(output_file, "wb") as f:
-        f.write(output_graph_def.SerializeToString())
-    print("%d ops in the optimized graph." % len(output_graph_def.node))
+    call([tf_path,
+          "--in_graph=" + graph_file,
+          "--out_graph=" + output_file,
+          "--inputs=" + input_names,
+          "--outputs=" + output_names,
+          """--transforms=
+          strip_unused_nodes(type=float, shape="1,299,299,3")
+          fold_constants(ignore_errors=true)
+          fold_batch_norms
+          fold_old_batch_norms"""])
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+            "Script freezes graph and optimize it for mobile usage")
     parser.add_argument(
-        "--model_folder",
+        "--model",
         type=str,
-        help="Path of folder with model to freez")
+        help="Path of folder + model name (folder_path/model_name)")
     parser.add_argument(
         "--input_names",
         type=str,
@@ -111,7 +75,13 @@ if __name__ == '__main__':
         type=str,
         default="",
         help="Output node names, comma separated.")
+    parser.add_argument(
+        "--tf_path",
+        type=str,
+        default="../../tensorflow/",
+        help="Path to the folder with tensorflow (requires bazel build of graph_transforms)")
+
     args = parser.parse_args()
 
-    graph = graph_freez(args.model_folder, args.output_names)
-    graph_optimization(graph, args.input_names, args.output_names)
+    graph = graph_freez(args.model, args.output_names)
+    graph_optimization(args.tf_path, graph, args.input_names, args.output_names)
