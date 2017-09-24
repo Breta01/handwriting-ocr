18928ff95df719ef7294f2675bbcf4da29013459
Breta01 <breta.hajek@gmail.com>
2017-09-24 17:50:46 +0200

    New method of loading more char images (around 6k)

--- ocr/datahelpers.py
+++ ocr/datahelpers.py
@@ -7,6 +7,44 @@ import glob
 import simplejson
 import cv2
 from .helpers import implt
+from .normalization import letterNorm
+
+
+CHARS_CZ = ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
+            'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z', 'Á', 'É', 'Í', 'Ó', 'Ú', 'Ý', 'á',
+            'é', 'í', 'ó', 'ú', 'ý', 'Č', 'č', 'Ď', 'ď', 'Ě',
+            'ě', 'Ň', 'ň', 'Ř', 'ř', 'Š', 'š', 'Ť', 'ť', 'Ů',
+            'ů', 'Ž', 'ž']
+
+CHARS_EN = ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
+            'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z']
+
+idxs = [i for i in range(len(CHARS_CZ))]
+idx_to_chars_cz = dict(zip(idxs, CHARS_CZ))
+chars_to_idx_cz = dict(zip(CHARS_CZ, idxs))
+
+idxs = [i for i in range(len(CHARS_EN))]
+idx_to_chars_en = dict(zip(idxs, CHARS_EN))
+chars_to_idx_en = dict(zip(CHARS_EN, idxs))
+
+def char2idx(c, lang='cz'):
+    if lang == 'en':
+        return chars_to_idx_en[c]
+    return chars_to_idx_cz[c]
+
+def idx2char(idx, lang='cz'):
+    if lang == 'en':
+        return idx_to_chars_en[idx]
+    return idx_to_chars_cz[idx]
+    
 
 def loadWordsData(dataloc='data/words/', loadGaplines=True, debug=False):
     """
@@ -48,7 +86,7 @@ def loadWordsData(dataloc='data/words/', loadGaplines=True, debug=False):
         assert len(labels) == len(images) == len(gaplines)
     else:
         assert len(labels) == len(images)
-    print("Number of Images:", len(labels))
+    print("-> Number of words:", len(labels))
 
     # Print one of the images (last one)
     if debug:
@@ -59,6 +97,73 @@ def loadWordsData(dataloc='data/words/', loadGaplines=True, debug=False):
     
     if loadGaplines:
         return (images, labels, gaplines)
+    return (images, labels)
+
+
+def words2chars(images, labels, gaplines):
+    """ Transform word images with gaplines into individual chars """
+    # Total number of chars
+    length = sum([len(l) for l in labels])
+    
+    imgs = np.empty(length, dtype=object)
+    newLabels = []
+    
+    height = images[0].shape[0]
+    
+    idx = 0;
+    for i, gaps in enumerate(gaplines):
+        for pos in range(len(gaps) - 1):
+            imgs[idx] = images[i][0:height, gaps[pos]:gaps[pos+1]]
+            newLabels.append(char2idx(labels[i][pos]))
+            idx += 1
+           
+    print("Loaded chars from words:", length)            
+    return imgs, newLabels
+
+
+def loadCharsData(charloc='data/charclas/', wordloc='data/words/', lang='cz', useWords=True):
+    """
+    Load chars images with corresponding labels
+    Input:
+        charloc      - char images FOLDER LOCATION
+        wordloc      - word images with gaplines FOLDER LOCATION
+    Returns: (images, labels)
+    """
+    print("Loading chars...")
+    # Get subfolders with chars
+    dirlist = glob.glob(charloc + lang + "/*/")
+    dirlist.sort()
+
+    if lang == 'en':
+        chars = CHARS_EN
+    else:
+        chars = CHARS_CZ
+    
+    images = np.zeros((1, 4096))
+    labels = []   
+
+    # For every label load images and create corresponding labels
+    # cv2.imread(img, 0) - for loading images in grayscale
+    # Images are scaled to 64x64 = 4096 px
+    for i in range(len(chars)):
+        imglist = glob.glob(dirlist[i] + '*.jpg')
+        imgs = np.array([letterNorm(cv2.imread(img, 0)) for img in imglist])
+        images = np.concatenate([images, imgs.reshape(len(imgs), 4096)])
+        labels.extend([i] * len(imgs))
+        
+    if useWords:    
+        imgs, words, gaplines = loadWordsData(wordloc)
+        imgs, chars = words2chars(imgs, words, gaplines)
+        
+        labels.extend(chars)
+        for i in range(len(imgs)):
+            images = np.concatenate([images,
+                                     letterNorm(imgs[i]).reshape(1, 4096)])            
+
+    images = images[1:]
+    labels = np.array(labels)
+    
+    print("-> Number of chars:", len(labels))
     return (images, labels)
 
 
