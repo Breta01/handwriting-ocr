4f6299cc08ca451385471e065b8c57574d1af2cd
Breta01 <breta.hajek@gmail.com>
2018-01-15 21:49:52 +0100

    Training Seq2Seq with 2px slider -> no big difference

--- ocr/mlhelpers.py
+++ ocr/mlhelpers.py
@@ -10,7 +10,7 @@ import csv
 
 class TrainingPlot:
     """
-    Creating live plot during training 
+    Creating live plot during training
     REUIRES notebook backend: %matplotlib notebook
     @TODO Migrate to Tensorboard
     """
@@ -23,54 +23,54 @@ class TrainingPlot:
     ax1 = None
     ax2 = None
     fig = None
-    
+
     def __init__(self, steps, testItr, lossItr):
         self.testInterval = testItr
         self.lossInterval = lossItr
         self.interval = steps
-              
+
         self.fig, self.ax1 = plt.subplots()
         self.ax2 = self.ax1.twinx()
         self.ax1.set_autoscaley_on(True)
         plt.ion()
-        
+
         self.updatePlot()
-        
+
         # Description
-        self.ax1.set_xlabel('iteration')
-        self.ax1.set_ylabel('train loss')
-        self.ax2.set_ylabel('test accuracy')
-        
+        self.ax1.set_xlabel('Iteration')
+        self.ax1.set_ylabel('Train Loss')
+        self.ax2.set_ylabel('Valid. Accuracy')
+
         # Axes limits
         self.ax1.set_ylim([0,10])
 
-        
-    def updatePlot(self):        
-        self.fig.canvas.draw()        
-        
+
+    def updatePlot(self):
+        self.fig.canvas.draw()
+
     def updateCost(self, lossTrain, index):
-        self.trainLoss.append(lossTrain)        
+        self.trainLoss.append(lossTrain)
         if len(self.trainLoss) == 1:
             self.ax1.set_ylim([0, min(10, math.ceil(lossTrain))])
         self.ax1.plot(self.lossInterval * np.arange(len(self.trainLoss)),
                       self.trainLoss, 'b', linewidth=1.0)
-        
+
         self.updatePlot()
-        
+
     def updateAcc(self, accVal, accTrain, index):
         self.validAcc.append(accVal)
         self.trainAcc.append(accTrain)
-                
+
         self.ax2.plot(self.testInterval * np.arange(len(self.validAcc)),
                       self.validAcc, 'r', linewidth=1.0)
         self.ax2.plot(self.testInterval * np.arange(len(self.trainAcc)),
                       self.trainAcc, 'g',linewidth=1.0)
-        
+
         self.ax2.set_title('Valid. Accuracy: {:.4f}'.format(self.validAcc[-1]))
-        
+
         self.updatePlot()
-        
-        
+
+
 class DataSet:
     """ Class for training data and feeding train function """
     images = None
@@ -89,7 +89,7 @@ class DataSet:
         """Return the next batch from the data set."""
         start = self.index
         self.index += batchSize
-        
+
         if self.index > self.length:
             # Shuffle the data
             perm = np.arange(self.length)
@@ -99,7 +99,7 @@ class DataSet:
             # Start next epoch
             start = 0
             self.index = batchSize
-        
+
 
         end = self.index
         return self.images[start:end], self.labels[start:end]
