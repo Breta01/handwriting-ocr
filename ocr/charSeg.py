fcbbe235a0f4f8e52325468fc0a9769c79df5225
Breta01 <breta.hajek@gmail.com>
2018-03-12 23:29:15 +0100

    Update GapClassificationDM, fix #17

--- ocr/charSeg.py
+++ ocr/charSeg.py
@@ -12,9 +12,8 @@ print("Loading Segmantation model:")
 segCNNGraph = Graph('models/gap-clas/CNN-CG')
 segRNNGraph = Graph('models/gap-clas/RNN/Bi-RNN-new', 'prediction')
 
-def classify(img, step=2, RNN=False):
-    slider = (60, 60)
-        
+def classify(img, step=2, RNN=False, slider=(60, 60)):
+    """ Slice the image and return raw output of classifier """
     length = (img.shape[1] - slider[1]) // 2 + 1
     if RNN:
         input_seq = np.zeros((1, length, slider[0]*slider[1]), dtype=np.float32)
@@ -36,20 +35,22 @@ def segmentation(img, step=2, RNN=False, debug=False):
     """
     Take preprocessed image of word
     and return array of positions separating chars - gaps
-    """        
-    slider = (60, 60)
-    length = (img.shape[1] - slider[1]) // 2 + 1
+    """
+    slider = (60, 30)
+    if RNN:
+        slider = (60, 60)
     
-    pred = classify(img, step, RNN)
+    # Run the classifier
+    pred = classify(img, step=step, RNN=RNN, slider=slider)
 
+    # Finalize the gap positions from raw prediction
     gaps = []
-
     lastGap = 0
     gapCount = 1
     gapPositionSum = slider[1] / 2
     firstGap = True
     gapBlockFirst = 0
-    gapBlockLast = slider[1]/2
+    gapBlockLast = slider[1] / 2
 
     for i, p in enumerate(pred):
         if p == 1:
@@ -75,7 +76,7 @@ def segmentation(img, step=2, RNN=False, debug=False):
     if gapBlockFirst != 0:
         gaps.append(int(gapBlockFirst))
     else:
-        gapPositionSum += (length - 1) * 2 + slider[1]/2
+        gapPositionSum += (len(pred) - 1) * 2 + slider[1]/2
         gaps.append(int(gapPositionSum / (gapCount + 1)))
         
     if debug:
@@ -84,7 +85,7 @@ def segmentation(img, step=2, RNN=False, debug=False):
         for gap in gaps:
             cv2.line(img,
                      ((int)(gap), 0),
-                     ((int)(gap), slider[1]),
+                     ((int)(gap), slider[0]),
                      (0, 255, 0), 1)
         implt(img, t="Separated characters")
         
