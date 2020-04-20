9a90765c0c7682a8b058ad0b41e7fd1fc57d1d16
Breta01 <breta.hajek@gmail.com>
2020-04-20 11:15:23 +0200

    Adding isort, new data control files

---
+++ handwriting_ocr/data/model_loader.py
@@ -0,0 +1,5 @@
+# Copyright 2020 Břetislav Hájek <info@bretahajek.com>
+# Licensed under the MIT License. See LICENSE for details.
+"""Modelu for downloading and providing pre-trained models."""
+
+from handwriting_ocr.data.data_loader import download_url, file_extract
